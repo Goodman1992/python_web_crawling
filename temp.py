1cfa0840cde5cf5ad595b005970df347b733c9cb
Victor <qingzhiyu@gmail.com>
2019-07-04 12:03:13 -0700

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,27 @@
+import requests, bs4
+
+def trim_title(str):
+    result=str.replace('\n','').replace('\xa0','').replace('[可播放]','')
+    return result
+
+all=[]
+for i in range(10):
+    url="https://movie.douban.com/top250?start=" + str(i*25) + '&filter='
+    res=requests.get(url)
+    bs=bs4.BeautifulSoup(res.text,"html.parser")
+    bs=bs.find('ol','grid_view')
+    source=bs.find_all('li')
+    for item in source:
+        temp=[]
+        temp.append(trim_title(item.find('div','hd').text).strip())
+        temp.append(item.find('span','inq').text)
+        temp.append(item.find('span','rating_num').text)
+        temp.append(item.find('div','hd').find('a')['href'])
+        all.append(temp)
+
+for i in range(len(all)):
+    print("Rank: "+str(i+1))
+    print("Title: "+all[i][0])
+    print("Comment: "+all[i][1])
+    print("Score: "+ all[i][2])
+    print("Url: "+all[i][3])
